564d3e0c6fd6e52090c6e1d66588fd6a91d8dfe6
Spenzor <spencercorwin@mac.com>
2018-01-22 22:43:17 -0800

    Create Chapter11ImageSiteDownloader.py

---
+++ Chapter11ImageSiteDownloader.py
@@ -0,0 +1,27 @@
+#! usr/bin/env python3
+#Chapter 11 Image Site Downloader
+#This script uses BeautifulSoup to download the first 20 images from a search of imgur.com
+import sys, requests, os, bs4
+
+print('Please enter search:')
+search= input()
+
+os.chdir('/Users/spencercorwin/Desktop')
+os.makedirs('imgur', exist_ok=True)
+res = requests.get('http://imgur.com/search?q='+str(search))
+res.raise_for_status()
+soup = bs4.BeautifulSoup(res.text)
+photoElem = soup.select('img')
+for i in range(20):
+    try:
+        photoUrl = 'http:' + photoElem[i].get('src')
+        print('Downloading image %s...' % (photoUrl))
+        res = requests.get(photoUrl)
+        res.raise_for_status()
+    except:
+        continue
+    #Save image to /Desktop/imgur
+    photoFile = open(os.path.join('imgur',os.path.basename(photoUrl)),'wb')
+    for chunk in res.iter_content(100000):
+        photoFile.write(chunk)
+    photoFile.close()
