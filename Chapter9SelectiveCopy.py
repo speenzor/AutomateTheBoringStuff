35256793b9fac9ec8eb0672938f5513dba282313
Speenzor <spencercorwin@mac.com>
2018-02-17 22:01:27 -0800

    Update Chapter9SelectiveCopy.py

--- Chapter9SelectiveCopy.py
+++ Chapter9SelectiveCopy.py
@@ -1,5 +1,7 @@
 #! usr/bin/env python3
-#Chapter 9 Selective Copy
+
+#Chapter 9 Challenge - Selective Copy
+
 #A program that walks through a folder tree and searches for files with a certain
 #file extention (such as .pdf or .jpg). Copy these files from their location
 #to a new folder.
