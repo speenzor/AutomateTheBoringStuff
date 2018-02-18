91fd5400170f4089d75df600ed110cc9b2a978b7
Speenzor <spencercorwin@mac.com>
2018-02-17 21:58:42 -0800

    Update Chapter4CommaCode.py

--- Chapter4CommaCode.py
+++ Chapter4CommaCode.py
@@ -12,3 +12,12 @@ def commacode(listvalue):
             return listvalue[0]
     listvalue.insert(-1,'and')
     return ', '.join(listvalue)
+
+#Another possible answer to the Chapter 4 Comma Code challenge
+def commaCode(list):
+    newString = ''
+    newString = newString + (list[0]) + ', '
+    for i in range(len(list)-1):
+        newString = newString + (list[i]) + ', '
+    newString = newString + 'and ' + (list[len(list)-1])
+    print (newString)
